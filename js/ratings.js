20fed48324ab2c32ad4aa29ad3a8414409985658
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-01 20:15:16 +0100

    Update ratings.js

---
+++ js/ratings.js
@@ -0,0 +1,25 @@
+
+const collect_ratings = ()=>{
+    
+    let ratings = {
+        average: 0,
+        count: 0,
+        sum: 0
+    };
+    let rating = 0;
+    const elements = document.querySelectorAll(".rating");
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace("star", ""))
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) *  rating;
+    });
+    if (ratings.count !== 0) {
+        return ratings.average = ratings.sum / ratings.count;
+    }
+    return ratings;
+};
+
+ document.addEventListener('change', () => {
+   const ratings = collect_ratings();
+   document.querySelector('#average').value = ratings.toFixed(2);
+ });
